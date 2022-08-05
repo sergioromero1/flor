d3ee8205b2e72fd55171296d30d297fb1ecf580e
Sergio <sar9206@gmail.com>
2022-08-05 10:58:40 -0500

    cambio 3

--- florapp/config/settings/base.py
+++ florapp/config/settings/base.py
@@ -8,7 +8,10 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
+
+hola hola hola
 """
+
 import os
 import environ
 from pathlib import Path
