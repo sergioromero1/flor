2a84a4eaf4d00e3b9425d3c0a1bf4c069e17c0be
Sergio <sar9206@gmail.com>
2022-08-09 15:39:20 -0500

    primeros cambios backend

--- florapp/users/views.py
+++ florapp/users/views.py
@@ -6,7 +6,7 @@ from django.views.generic  import FormView
 from users.forms import SignUpForm
 
 class LoginView(LoginView):
-    template_name = 'users/login.html'
+    template_name = 'orders/login.html'
 
 class LogoutView(LoginRequiredMixin, LogoutView):
     template_name = 'users/logout.html'
@@ -14,7 +14,7 @@ class LogoutView(LoginRequiredMixin, LogoutView):
 class SignUpView(FormView):
     form_class = SignUpForm
     success_url = reverse_lazy('users:login')
-    template_name = 'users/signup.html'
+    template_name = 'orders/register.html'
 
     def form_valid(self, form):
         "Safe form data"
